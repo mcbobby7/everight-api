e60c4edcb3a6f1d39636071867cb3509f9d0f72e
mcbobby <madumcbobby@yahoo.com>
2022-06-17 12:00:16 +0100

    fix deploy issue

--- controllers/emailFork.js
+++ controllers/emailFork.js
@@ -12,64 +12,69 @@ process.on("message", (message) => {
 });
 
 const sendEmail = async (userData, messageId, senderName) => {
-  try {
-    let pool = await mssql.connect(sqlConfig);
-    let messageTemp = await pool
-      .request()
-      .query(`select * from [dbo].[DocTemplates] where Id = ${messageId}`);
-    let messageContent;
-    if (messageTemp.recordset.length > 0) {
-      messageContent = messageTemp.recordset[0].Template_Text;
-    } else {
-      process.send("error");
-    }
+  if (senderName) {
+    try {
+      let pool = await mssql.connect(sqlConfig);
+      let messageTemp = await pool
+        .request()
+        .query(`select * from [dbo].[DocTemplates] where Id = ${messageId}`);
+      let messageContent;
+      if (messageTemp.recordset.length > 0) {
+        messageContent = messageTemp.recordset[0].Template_Text;
+      } else {
+        process.send("error");
+      }
 
-    process.send("confirm");
-    for (let i = 0; i < userData.length; i++) {
-      if (userData[i].EmailAddress) {
-        let date = moment(new Date()).format("YYYY");
-        let name = userData[i].PatientName;
-        let age = +date - +userData[i].DateOfBirth.substring(0, 4);
-        let gender = userData[i].Gender;
-        let DOB = moment(new Date(userData[i].DateOfBirth)).format(
-          "DD-MM-YYYY"
-        );
-        messageContent = messageContent
-          .replace("[[name]]", name, 8)
-          .replace("[[age]]", age, 8)
-          .replace("[[sex]]", gender, 8)
-          .replace("[[DOB]]", DOB, 8);
-        if (senderName === "email") {
-          const body = {
-            receiver: userData[i].EmailAddress,
-            msg: messageContent,
-            subject: messageTemp.recordset[0].Name,
-          };
+      process.send("confirm");
+      for (let i = 0; i < userData.length; i++) {
+        if (userData[i].EmailAddress) {
+          let date = moment(new Date()).format("YYYY");
+          let name = userData[i].PatientName;
+          let age = +date - +userData[i].DateOfBirth.substring(0, 4);
+          let gender = userData[i].Gender;
+          let DOB = moment(new Date(userData[i].DateOfBirth)).format(
+            "DD-MM-YYYY"
+          );
+          messageContent = messageContent
+            .replace("[[name]]", name, 8)
+            .replace("[[age]]", age, 8)
+            .replace("[[sex]]", gender, 8)
+            .replace("[[DOB]]", DOB, 8);
+          if (senderName === "email") {
+            const body = {
+              receiver: userData[i].EmailAddress,
+              msg: messageContent,
+              subject: messageTemp.recordset[0].Name,
+            };
 
-          await axios
-            .post("http://portal.everightlab.com/notification/sendemail", body)
-            .then((response) => {})
-            .catch((error) => {
-              console.log(error);
-            });
-        } else {
-          const body = {
-            phoneno: userData[i].PhoneNo,
-            msg: messageContent.replace(/<[^>]*>?/gm, ""),
-          };
-          await axios
-            .post("http://portal.everightlab.com/notification/sendsms", body)
-            .then((response) => {})
-            .catch((error) => {
-              console.log(error);
-            });
+            await axios
+              .post(
+                "http://portal.everightlab.com/notification/sendemail",
+                body
+              )
+              .then((response) => {})
+              .catch((error) => {
+                console.log(error);
+              });
+          } else {
+            const body = {
+              phoneno: userData[i].PhoneNo,
+              msg: messageContent.replace(/<[^>]*>?/gm, ""),
+            };
+            await axios
+              .post("http://portal.everightlab.com/notification/sendsms", body)
+              .then((response) => {})
+              .catch((error) => {
+                console.log(error);
+              });
+          }
         }
       }
+    } catch (error) {
+      console.log(error);
+      mssql.close;
+      process.send("error");
+      return false;
     }
-  } catch (error) {
-    console.log(error);
-    mssql.close;
-    process.send("error");
-    return false;
   }
 };
--- database.js
+++ database.js
@@ -1,9 +1,25 @@
+// export const sqlConfig = {
+//   user: "superdbuser",
+//   password: "J@p@n123",
+//   database: "hmsdb",
+//   server: "mssql-35101-0.cloudclusters.net",
+//   port: 35101,
+//   pool: {
+//     max: 10,
+//     min: 0,
+//     idleTimeoutMillis: 30000,
+//   },
+//   options: {
+//     encrypt: true, // for azure
+//     trustServerCertificate: true,
+//   },
+// };
 export const sqlConfig = {
-  user: "superdbuser",
-  password: "J@p@n123",
+  user: "sa",
+  password: "Welcome@1234",
   database: "hmsdb",
-  server: "mssql-35101-0.cloudclusters.net",
-  port: 35101,
+  server: `db-server\\sqlexpress`,
+  port: 1433,
   pool: {
     max: 10,
     min: 0,
